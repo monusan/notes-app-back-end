4c4551e103446a19bbe363659397606e4dd6dc12
monusan <monusan@gmail.com>
2023-05-12 13:29:57 +0700

    data validation implemented

---
+++ src/validator/notes/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const InvariantError = require('../../exceptions/InvariantError');
+
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
---
+++ src/validator/notes/schema.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+const Joi = require('joi');
+
+const NotePayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  body: Joi.string().required(),
+  tags: Joi.array().items(Joi.string()).required(),
+});
+
+module.exports = { NotePayloadSchema };
